91bf819be5d8d7a67a01af159d86b7bfe320b855
Wenlin Huang <jackhwl@hotmail.com>
2019-06-04 19:05:18 -0400

    fix login redirect

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -4,32 +4,37 @@ import { UserService, MenuService } from '../../core/services';
 
 @Component({
     templateUrl: './login.component.html',
-    styleUrls:['./login.component.css']
+    styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent implements OnInit { 
-    email: string;
-    password1: string;
-    constructor(private userSVC: UserService, private menuSVC: MenuService, private router: Router){}
+export class LoginComponent implements OnInit {
+    public email: string;
+    public password1: string;
+    constructor(private userSVC: UserService,
+                private menuSVC: MenuService,
+                private router: Router) {}
 
     public ngOnInit() {
         if (!this.menuSVC.topMenu) {
-           this.menuSVC.setTopNav('admin', null);
-           this.menuSVC.getMisc();
+            this.menuSVC.setTopNav('admin', null);
+            this.menuSVC.getMisc();
         }
-      }
-    
-    login(){
-        this.userSVC.login(this.email, this.password1);
-        this.userSVC.verifyUser();
     }
 
-    signup(){
+    public login() {
+        this.userSVC.login(this.email, this.password1)
+        .then(() => {
+            this.userSVC.verifyUser();
+            this.onSaveComplete();
+        });
+    }
+
+    public signup() {
         this.router.navigate(['/admin/signup']);
     }
 
-    cancel(){
-        this.router.navigate(['']);
+    public cancel() {
+        this.onSaveComplete();
     }
 
     public onSaveComplete(): void {
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -7,72 +7,74 @@ import { AngularFireDatabase } from '@angular/fire/database';
 
 @Injectable()
 export class UserService implements CanActivate {
-    userLoggedIn: boolean = false;
-    loggedInUser: string;
-    authUser: any;
+    public userLoggedIn: boolean = false;
+    public loggedInUser: string;
+    public authUser: any;
 
-    constructor(private router: Router, private db: AngularFireDatabase, private au: AngularFireAuth ) {
-        //firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+    constructor(private router: Router,
+                private db: AngularFireDatabase,
+                private au: AngularFireAuth ) {
+                // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
     }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot ): boolean {
-        let url: string = state.url;
+    public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot ): boolean {
+        const url: string = state.url;
         return this.verifyLogin(url);
     }
 
-    verifyLogin(url: string): boolean {
+    public verifyLogin(url: string): boolean {
         if (this.userLoggedIn) { return true; }
 
         this.router.navigate(['/admin/login']);
         return false;
     }
 
-    register(email: string, password: string){
+    public register(email: string, password: string) {
         // this.au.auth.createUserWithEmailAndPassword(email, password)
         //     .catch(function(error) {
         //         alert(`${error.message} Please Try Again!`);
         //     });
     }
 
-    verifyUser() {
+    public verifyUser() {
         console.log('verifyUser');
-        this.authUser = this.au.auth.currentUser; //firebase.auth().currentUser;
+        this.authUser = this.au.auth.currentUser; // firebase.auth().currentUser;
 
         if (this.authUser) {
-            alert(`Welcome ${this.authUser.email}`);
+            // alert(`Welcome ${this.authUser.email}`);
             this.loggedInUser = this.authUser.email;
             this.userLoggedIn = true;
-            this.router.navigate(['/admin']);
+            //this.router.navigate(['/admin']);
         }
     }
 
-    login(loginEmail: string, loginPassword: string){
-        let vm = this;
-        this.au.auth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    public login(loginEmail: string, loginPassword: string) {
+        const vm = this;
+        return this.au.auth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
         .then(function() {
           // Existing and future Auth states are now persisted in the current
           // session only. Closing the window would clear any existing state even
           // if a user forgets to sign out.
           // ...
           // New sign-in will be persisted with session persistence.
-          vm.au.auth.signInWithEmailAndPassword(loginEmail, loginPassword)
+          return vm.au.auth.signInWithEmailAndPassword(loginEmail, loginPassword)
           .catch(function(error) {
               alert('${error.message} Unable to login. Try Again!');
           });
 
-          //return firebase.auth().signInWithEmailAndPassword(email, password);
+          // return firebase.auth().signInWithEmailAndPassword(email, password);
         })
         .catch(function(error) {
           // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
+          // let errorCode = error.code;
+          // let errorMessage = error.message;
           console.error(error);
         });
     }
 
-    logout() {
+    public logout() {
         this.userLoggedIn = false;
-        this.au.auth.signOut().then(function(){
+        this.au.auth.signOut().then(function() {
             console.log('Logged Out');
         }, function(error) {
             alert('${error.message} Unabled to logout. Try again!');
