eea4263aac2eaa28ae19ca64383cc26397ec6afa
jackhwl <jackhwl@hotmail.com>
2019-06-09 17:47:48 -0400

    Converting from Arrays to Trees

--- obserableEx.js
+++ obserableEx.js
@@ -1,3 +1,52 @@
+// Exercise 25: Converting from Arrays to Trees
+// When information is organized in a tree like a JSON expression, relationships point from parent to child. In relational systems like databases, relationships point from children to their parents. Both ways of organizing information are equivalent, and depending on the circumstances, we might get data organized in one way or another. It may surprise you to learn that you can use the 5 query functions you already know to easily convert between these representations. In other words, not only can you query arrays from trees, you can query trees from arrays.
+// We have 2 arrays each containing lists, and videos respectively. Each video has a listId field indicating its parent list. We want to build an array of list objects, each with a name and a videos array. The videos array will contain the video's id and title. In other words we want to build the following structure:
+
+function ex25() {
+	var lists = [
+			{
+				"id": 5434364,
+				"name": "New Releases"
+			},
+			{
+				"id": 65456475,
+				"name": "Thrillers"
+			}
+		],
+		videos = [
+			{
+				"listId": 5434364,
+				"id": 65432445,
+				"title": "The Chamber"
+			},
+			{
+				"listId": 5434364,
+				"id": 675465,
+				"title": "Fracture"
+			},
+			{
+				"listId": 65456475,
+				"id": 70111470,
+				"title": "Die Hard"
+			},
+			{
+				"listId": 65456475,
+				"id": 654356453,
+				"title": "Bad Boys"
+			}
+		];
+
+    return lists.map(l => ({name: l.name, videos: videos.filter(v=>v.listId==l.id)
+      .map(v => ({id: v.id, title: v.title}))}));
+    // return lists.map(l => ({name: l.name, videos: videos.filter(v=>v.listId==l.id)
+    // .map(v => ({id: v.id, title: v.title}))}));
+  //return lists.concatMap(l => Array.zip([l], [{vs: videos.filter(v=>v.listId==l.id).
+  //  map(v => ({id: v.id, title: v.title}))}],
+  //  (l, vs1) => ({name: l.name, videos: vs1.vs})
+
+  //                )) // complete this expression
+}
+
 // Exercise 24: Retrieve each video's id, title, middle interesting moment time, and smallest box art url.
 // This is a variation of the problem we solved earlier. This time each video has an interesting moments collection, each representing a time during which a screenshot is interesting or representative of the title as a whole. Notice that both the boxarts and interestingMoments arrays are located at the same depth in the tree. Retrieve the time of the middle interesting moment and the smallest box art url simultaneously with zip(). Return an {id, title, time, url} object for each video.
 
