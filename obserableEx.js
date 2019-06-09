4495b4431d7c17d931d6acccf68dde6c3bd34898
jackhwl <jackhwl@hotmail.com>
2019-06-09 08:54:58 -0400

    zip reduce

--- obserableEx.js
+++ obserableEx.js
@@ -1,3 +1,97 @@
+// Exercise 24: Retrieve each video's id, title, middle interesting moment time, and smallest box art url.
+// This is a variation of the problem we solved earlier. This time each video has an interesting moments collection, each representing a time during which a screenshot is interesting or representative of the title as a whole. Notice that both the boxarts and interestingMoments arrays are located at the same depth in the tree. Retrieve the time of the middle interesting moment and the smallest box art url simultaneously with zip(). Return an {id, title, time, url} object for each video.
+
+function ex24() {
+	var movieLists = [
+			{
+				name: "New Releases",
+				videos: [
+					{
+						"id": 70111470,
+						"title": "Die Hard",
+						"boxarts": [
+							{ width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"interestingMoments": [
+							{ type: "End", time:213432 },
+							{ type: "Start", time: 64534 },
+							{ type: "Middle", time: 323133}
+						]
+					},
+					{
+						"id": 654356453,
+						"title": "Bad Boys",
+						"boxarts": [
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+							{ width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"interestingMoments": [
+							{ type: "End", time:54654754 },
+							{ type: "Start", time: 43524243 },
+							{ type: "Middle", time: 6575665}
+						]
+					}
+				]
+			},
+			{
+				name: "Instant Queue",
+				videos: [
+					{
+						"id": 65432445,
+						"title": "The Chamber",
+						"boxarts": [
+							{ width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"interestingMoments": [
+							{ type: "End", time:132423 },
+							{ type: "Start", time: 54637425 },
+							{ type: "Middle", time: 3452343}
+						]
+					},
+					{
+						"id": 675465,
+						"title": "Fracture",
+						"boxarts": [
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+							{ width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+							{ width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"interestingMoments": [
+							{ type: "End", time:45632456 },
+							{ type: "Start", time: 234534 },
+							{ type: "Middle", time: 3453434}
+						]
+					}
+				]
+			}
+		];
+
+	//------------ COMPLETE THIS EXPRESSION --------------
+	return movieLists.
+						concatMap(m => m.videos
+             	.concatMap(v=> Array.zip( v.boxarts
+              							.reduce((acc, curr) => acc.width * acc.height < curr.width * curr.height
+      																	? acc : curr),
+                                 v.interestingMoments.filter(i=>i.type==='Middle'),
+                                   (box, inte) => ({id: v.id, title: v.title, time: inte.time, url: box.url})
+                             )
+             						)
+
+             );
+
+}
+
 // Exercise 21: Combine videos and bookmarks by index
 // Use a for loop to traverse the videos and bookmarks array at the same time. For each video and bookmark pair, create a {videoId, bookmarkId} pair and add it to the videoIdAndBookmarkIdPairs array.
 function ex21() {
