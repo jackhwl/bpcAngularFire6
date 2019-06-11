5870ed08bb96455c28bf75eb6617ee05506a59ca
jackhwl <jackhwl@hotmail.com>
2019-06-11 07:02:03 -0400

    Converting from Arrays to Deeper Trees

--- obserableEx.js
+++ obserableEx.js
@@ -1,3 +1,103 @@
+// Exercise 26: Converting from Arrays to Deeper Trees
+// Let's try creating a deeper tree structure. This time we have 4 separate arrays each containing lists, videos, boxarts, and bookmarks respectively. Each object has a parent id, indicating its parent. We want to build an array of list objects, each with a name and a videos array. The videos array will contain the video's id, title, bookmark time, and smallest boxart url. In other words we want to build the following structure:
+[
+	{
+		"name": "New Releases",
+		"videos": [
+			{
+				"id": 65432445,
+				"title": "The Chamber",
+				"time": 32432,
+				"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg"
+			},
+			{
+				"id": 675465,
+				"title": "Fracture",
+				"time": 3534543,
+				"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture120.jpg"
+			}
+		]
+	},
+	{
+		"name": "Thrillers",
+		"videos": [
+			{
+				"id": 70111470,
+				"title": "Die Hard",
+				"time": 645243,
+				"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg"
+			},
+			{
+				"id": 654356453,
+				"title": "Bad Boys",
+				"time": 984934,
+				"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg"
+			}
+		]
+	}
+]
+function ex26() {
+	var lists = [
+			{
+				"id": 5434364,
+				"name": "New Releases"
+			},
+			{
+				"id": 65456475,
+				name: "Thrillers"
+			}
+		],
+		videos = [
+			{
+				"listId": 5434364,
+				"id": 65432445,
+				"title": "The Chamber"
+			},
+			{
+				"listId": 5434364,
+				"id": 675465,
+				"title": "Fracture"
+			},
+			{
+				"listId": 65456475,
+				"id": 70111470,
+				"title": "Die Hard"
+			},
+			{
+				"listId": 65456475,
+				"id": 654356453,
+				"title": "Bad Boys"
+			}
+		],
+		boxarts = [
+			{ videoId: 65432445, width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+			{ videoId: 65432445, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" },
+			{ videoId: 675465, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+			{ videoId: 675465, width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+			{ videoId: 675465, width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+			{ videoId: 70111470, width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+			{ videoId: 70111470, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" },
+			{ videoId: 654356453, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+			{ videoId: 654356453, width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+		],
+		bookmarks = [
+			{ videoId: 65432445, time: 32432 },
+			{ videoId: 675465, time: 3534543 },
+			{ videoId: 70111470, time: 645243 },
+			{ videoId: 654356453, time: 984934 }
+		];
+
+  return lists.map(l => ({name:l.name , videos: videos.filter(v=> v.listId === l.id)
+    													.concatMap(v=> Array.zip(boxarts.filter(b=> b.videoId===v.id)
+    																											.reduce((acc, curr)=> acc.width*acc.height<curr.width*curr.height?acc:curr),
+                                               	 bookmarks.filter(m=>m.videoId===v.id),
+                                                  (b, m) => ({time:m.time, boxart: b.url}))
+                                                    .map(bb=> ({id: v.id, title: v.title, time: bb.time, boxart: bb.boxart}))
+                                                      )})
+
+    ) // complete this expression
+
+}
 // Exercise 25: Converting from Arrays to Trees
 // When information is organized in a tree like a JSON expression, relationships point from parent to child. In relational systems like databases, relationships point from children to their parents. Both ways of organizing information are equivalent, and depending on the circumstances, we might get data organized in one way or another. It may surprise you to learn that you can use the 5 query functions you already know to easily convert between these representations. In other words, not only can you query arrays from trees, you can query trees from arrays.
 // We have 2 arrays each containing lists, and videos respectively. Each video has a listId field indicating its parent list. We want to build an array of list objects, each with a name and a videos array. The videos array will contain the video's id and title. In other words we want to build the following structure:
