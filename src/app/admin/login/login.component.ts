1711315c0eebd89285bc52aee8baa7e51ff937cd
jackhwl <jackhwl@hotmail.com>
2019-06-16 21:57:28 -0400

    fix login page issue

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,25 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService, MenuService } from '../../core/services';
+import { UserService } from '../../core/services';
 
 @Component({
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent implements OnInit {
+export class LoginComponent {
     public email: string;
     public password1: string;
-    constructor(private userSVC: UserService,
-                private menuSVC: MenuService,
-                private router: Router) {}
-
-    public ngOnInit() {
-        if (!this.menuSVC.topMenu) {
-            this.menuSVC.setTopNav('admin', null);
-            this.menuSVC.getMisc();
-        }
-    }
+    constructor(private userSVC: UserService, private router: Router) {}
 
     public login() {
         this.userSVC.login(this.email, this.password1)
